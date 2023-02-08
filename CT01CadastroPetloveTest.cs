ac6b4a8ff13a061f1c3f3ef2cb00aba09b4592de
TIAGO SANTANA <75752302+tiagodevsantana@users.noreply.github.com>
2023-02-08 15:09:50 -0300

    Update CT01CadastroPetloveTest.cs

--- CT01CadastroPetloveTest.cs
+++ CT01CadastroPetloveTest.cs
@@ -16,11 +16,19 @@ public class SuiteTests : IDisposable {
 
   [Fact]
   public void CT01CadastroPetlove() {
+
+    //Acesso ao site da Petlove
+    
     driver.Navigate().GoToUrl("https://www.petlove.com.br/");
     driver.Manage().Window.Size = new System.Drawing.Size(1382, 736);
+
+    //Inicio da criação do cadastro
+    
     driver.FindElement(By.CssSelector(".header-item__user > .col")).Click();
     Assert.Equal(driver.FindElement(By.Id("create-account-button")).Text, "Crie uma agora!");
     driver.FindElement(By.Id("create-account-button")).Click();
+
+    //Validação dos campos do cadastro
     {
       IReadOnlyCollection<IWebElement> elements = driver.FindElements(By.Id("email"));
       Assert.True(elements.Count > 0);
@@ -47,13 +55,22 @@ public class SuiteTests : IDisposable {
     }
     Assert.Equal(driver.FindElement(By.CssSelector(".form-button--fixed:nth-child(6)")).Text, "Criar minha conta");
 
-    driver.FindElement(By.Id("email")).SendKeys("tiagoteste1@gmail.com");
+  //Preenchimento dos campos
+
+    driver.FindElement(By.Id("email")).SendKeys("tiagoteste100@gmail.com");
     driver.FindElement(By.Id("first-name")).SendKeys("Tiago");
     driver.FindElement(By.Id("last-name")).SendKeys("Santana");
     driver.FindElement(By.Id("document")).SendKeys("85671294002");
     driver.FindElement(By.Id("phone")).SendKeys("71999999999");
     driver.FindElement(By.Id("password")).SendKeys("Petlove123*");
     driver.FindElement(By.Id("submit-btn")).Click();
+
+  //Login na plataforma Petlove
+    
+    driver.Navigate().GoToUrl("https://www.petlove.com.br/");
+    driver.Manage().Window.Size = new System.Drawing.Size(1382, 736);
+    driver.FindElement(By.type("email")).SendKeys("tiagoteste100@gmail.com");
+    driver.FindElement(By.name("password")).SendKeys("Petlove123*");
+    
   }
 }
-
